1fc95de147d24df206ae17413f03ae83d2cc2d90
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-11-25 15:27:19 +0530

    dev commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { ViewPostComponent } from './view-post/view-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AddPostComponent,
+    ViewPostComponent
   ],
   imports: [
     BrowserModule,
